97451a6bc81e6e6dbb69b3c5a995aa98aa82616d
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2019-03-07 10:26:40 -0800

    updates

---
+++ Game/Assets/Computer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Computer : MonoBehaviour {
+
+// Start is called before the first frame update
+void Start()
+    {
+        //Debug.Log("this wont end well");
+    }
+    void OnMouseDown(){
+        Debug.Log("wow!");
+        this.gameObject.transform.Find("ComputerMenu").gameObject.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Game/Assets/ComputerPressedMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerPressedMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+    public static void ShowMenu() {
+        Debug.Log("set show to trye");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
